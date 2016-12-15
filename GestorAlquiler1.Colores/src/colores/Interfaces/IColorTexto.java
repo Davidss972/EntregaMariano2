fa0ebb7bbbb4f4f7547cca6caf44c1e56531048f
Davidss972 <davidss97@gmail.com>
2016-12-15 13:30:12 +0100

    proyecto Colores
    Incluye la clase que da color a las cadenas de carácteres del programa

---
+++ GestorAlquiler1.Colores/src/colores/Interfaces/IColorTexto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colores.Interfaces;
+
+/**
+ *
+ * @author gokud
+ */
+public interface IColorTexto
+{
+
+    void print(String color, String txt);
+    void println(String color, String txt);
+    String getStringInColor(String color, String txt);
+}
